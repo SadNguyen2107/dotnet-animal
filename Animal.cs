4d68de94f783bad5359acdf48bba9afe52ac5120
SadNguyen2107 <nguyentrungson217@gmail.com>
2024-01-05 20:29:44 +0700

    First Commit

---
+++ Animal.cs
@@ -0,0 +1,16 @@
+namespace SN_Program
+{
+    public abstract class Animal
+    {
+        protected string? Weight { get; set; }
+        protected string? Height { get; set; }
+
+        public Animal(string weight, string height)
+        {
+            this.Weight = weight;
+            this.Height = height;
+        }
+
+        public abstract void PrintInfo();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SN_Program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Cat cat = new Cat("20kg", "1.5", "kitty");
+            cat.PrintInfo();
+        }
+    }
+}
